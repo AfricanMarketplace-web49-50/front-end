4710ba1baec0c2611b2b60677f73730acda73a45
iamleahb <leah.ball@gmail.com>
2022-02-01 12:44:23 -0600

    basic form created

---
+++ african-marketplace/src/components/NavBar/navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./navbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">
+          <h1>African Marketplace</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/categories" activeStyle>
+            Categories
+          </NavLink>
+          <NavLink to="/shop" activeStyle>
+            Shop
+          </NavLink>
+          <NavLink to="/sign-up" activeStyle>
+            Sign Up
+          </NavLink>
+          <NavBtnLink to="/login">Login</NavBtnLink>
+        </NavMenu>
+        {/* <NavBtn>
+                    <NavBtnLink to='/login'>Sign In</NavBtnLink>
+                </NavBtn> */}
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
