e7426045f1cdedc2bce3cb688f151219654a33ff
iamleahb <leah.ball@gmail.com>
2022-02-03 14:06:03 -0600

    created styled components for Shop page

---
+++ african-marketplace/src/components/Shop/ShopElements.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const ListOfItemsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2%;
+  width: 80%;
+`;
+export const ListHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: yellow;
+  width: 75%;
+  justify-content: space-around;
+  padding: 2%;
+  margin-bottom: 2%;
+  align-items: center;
+`;
+
+export const ItemHeader = styled.h1`
+  /* padding: 2%; */
+  font-size: 3rem;
+`;
+
+export const Button = styled(Link)`
+  display: flex;
+  border: none;
+  outline: none;
+  background: #256ce1;
+  white-space: nowrap;
+  color: white;
+  padding: 2% 4%;
+  border-radius: 15px;
+  text-decoration: none;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    background: #fff; //#d4d977 Vomit Green
+    color: #010606;
+  }
+`;
+
+export const ItemCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-bottom: 1px solid blue;
+  width: 65%;
+  justify-content: space-around;
+  padding: 2%;
+  margin: 2%;
+`;
+
+export const ItemName = styled.h2`
+  font-size: 1.5rem;
+`;
+
+export const ItemPrice = styled.p`
+  font-size: 1rem;
+`;
